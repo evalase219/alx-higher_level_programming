2c2709916449b9c5f1bc9a0cab9e174c802dccc5
evalase219 <chiomaevangeline85@gmail.com>
2023-08-17 13:24:03 -0700

    Return a new dictionary with all values multiply by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    """Return a new dictionary with all values multiply by 2"""
+    new_dic = a_dictionary.copy()
+    list_keys = list(new_dic.keys())
+    for element in list_keys:
+        new_dic[element] *= 2
+    return (new_dic)
