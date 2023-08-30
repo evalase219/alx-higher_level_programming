d8115fb5713a67a386e73e6fb0c56a6984342c51
evalase219 <chiomaevangeline85@gmail.com>
2023-08-30 13:58:27 -0700

    Definition of square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialization of a new square.
+        Args:
+            size(int): equal length of a square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise TypeError("size must be >= 0")
+        self.__size = size
