a9dc839d39d405b59da69b28d82637c5aff175d1
evalase219 <chiomaevangeline85@gmail.com>
2023-09-13 14:38:23 -0700

    Replace all the attributes of the student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Defines a class student."""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve the dictionary representation of the student.
+
+        Arg:
+            attrs (list): The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all the attributes of the student.
+
+        Arg:
+            json (dict): The key/value pairs to replace attributes with
+        """
+        for i, j in json.items():
+            setattr(self, i, j)
