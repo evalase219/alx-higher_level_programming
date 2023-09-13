64c5bc21dcd070a4bda7df120a1f026ff8b2a193
evalase219 <chiomaevangeline85@gmail.com>
2023-09-13 05:07:22 -0700

    Define a class student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -13,6 +13,9 @@ class Student:
             last_name (str): The last name of the student.
             age (int): The age of the student.
         """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
         def to_json(self):
             """Get a dictionary representation of the student."""
