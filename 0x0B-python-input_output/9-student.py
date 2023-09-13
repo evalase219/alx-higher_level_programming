9554d1af67905734d870cacc8302dd7969df700f
evalase219 <chiomaevangeline85@gmail.com>
2023-09-13 04:38:18 -0700

    Define  class student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a class student."""
+
+
+class Student:
+    """Represent a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+
+        def to_json(self):
+            """Get a dictionary representation of the student."""
+            return self.__dict__
