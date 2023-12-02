79a94d5bb743a61b34936720fc61a120d19379c9
evalase219 <chiomaevangeline85@gmail.com>
2023-12-01 21:06:59 -0800

    Curl body size

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# script that takes in a URL, sends a request to that URL and display the size
+
+curl -sI $1 | grep "Content-length" | cut -d " " -f2
---
+++ 0x10-python-network_0/README.md
@@ -0,0 +1,1 @@
+My readme file in 0x10-python-network_0
